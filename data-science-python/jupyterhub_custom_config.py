70df89892983f005e5c0743c748306b13b6b121e
Csaba Sidló <csaba.sidlo@gmail.com>
2021-10-18 22:03:49 +0200

    jupyterhub custom config file update to match spark version

--- data-science-python/jupyterhub_custom_config.py
+++ data-science-python/jupyterhub_custom_config.py
@@ -3,6 +3,6 @@ c.Spawner.environment = {
   'SPARK_HOME': '/usr/local/spark',
   'HADOOP_VERSION': '3.2',
   'SHELL': '/bin/bash',
-  'SPARK_OPTS': '--driver-java-options=-Xmx8000M -XX:-UseGCOverheadLimit --driver-java-options=-Dlog4j.logLevel=info',
-  'APACHE_SPARK_VERSION': '3.0.1'
+  'SPARK_OPTS': '--driver-java-options=-Xmx8000M -XX:-UseGCOverheadLimit -Dlog4j.logLevel=info',
+  'APACHE_SPARK_VERSION': '3.1.2'
 }
