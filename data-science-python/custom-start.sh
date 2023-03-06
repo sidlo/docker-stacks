8e28ce1f98503bed3033ad93a47f8652ae3ba405
David Balamber <bdavid@ilab.sztaki.hu>
2023-03-06 13:08:39 +0000

    Changed conda solver to libmamba. Added bqplot as conda package instead of JupyterLab extension. Added jupyterlab-lsp to replace jupyterlab_go_to_definition. Added jupyterlab-geojson as conda package instead of JupyterLab extension. Added retrolab (classic interface for JupyterLab). Changed plotly's channel from plotly to conda-forge. Removed qgrid. Removed some JupyterLab extensions (bokeh, plotly), some are no longer required, only installing conda packages suffice. Removed vega2-extension: new version included in JupyterLab 3.

--- data-science-python/custom-start.sh
+++ data-science-python/custom-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ "${ES_ENABLED}" == "true" ]]; then
-    service elasticsearch start
+    /etc/init.d/elasticsearch start
 fi
 if [[ "${JUPYTERHUB_ENABLED}" == "true" ]]; then
     jupyterhub -f jupyterhub_custom_config.py > jupyterhub.log 2>&1 &
